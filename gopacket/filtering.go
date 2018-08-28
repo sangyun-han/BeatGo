fbb941814e17046a990de486f5875cb78f85a7ed
sangyun-han <sangyun628@gmail.com>
2018-08-28 17:26:26 +0900

    add gopacket example

---
+++ gopacket/filtering.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"log"
+	"time"
+)
+
+var (
+	device       string = "enp4s0"
+	snapshot_len int32  = 1024
+	promiscuous  bool   = false
+	err          error
+	timeout      time.Duration = 5 * time.Second
+	handle       *pcap.Handle
+)
+
+func main() {
+	// Open device for live capture
+	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	// Set filter
+	var filter string = "tcp and port 80"
+	err = handle.SetBPFFilter(filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Only capturing TCP port 80 packets.")
+
+	// Use the handle as a packet source to process all packets
+	packetSrc := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSrc.Packets() {
+		fmt.Println(packet)
+	}
+}
